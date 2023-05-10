c1e802e370b1ff64209ebb8c1cf3db928a10679c
hzg94 <66016605+hzg94@users.noreply.github.com>
2023-05-10 11:22:03 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'dumi';
 import path from 'path';
 
 export default defineConfig({
-  srcTranspiler: 'esbuild',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'anydesign',
