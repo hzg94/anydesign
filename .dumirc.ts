cd3572223d91721cfa8626b233983ce9506fb3bf
hzg94 <66016605+hzg94@users.noreply.github.com>
2023-05-10 11:19:53 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'dumi';
 import path from 'path';
 
 export default defineConfig({
-  codeSplitting: {
-    jsStrategy: 'granularChunks'
-  },
   srcTranspiler: 'esbuild',
   outputPath: 'docs-dist',
   themeConfig: {
