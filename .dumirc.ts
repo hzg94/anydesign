32c9f2b4aeb0da8d77fc0d5993b2279cad4076e1
hzg94 <66016605+hzg94@users.noreply.github.com>
2023-05-10 10:57:05 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 import path from 'path';
 
 export default defineConfig({
+  codeSplitting: 'depPerChunk',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'anydesign',
